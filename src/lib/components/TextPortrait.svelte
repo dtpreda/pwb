b9c4845187853d3564785fbc01598c29a5effb42
David Preda <dtpreda30@gmail.com>
2023-09-15 09:20:04 +0100

    Start homepage

---
+++ src/lib/components/TextPortrait.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { onMount } from "svelte";
+
+    export let src = "";
+    export let labelText = "";
+    /** @type {HTMLImageElement} */
+    let image;
+    /** @type {HTMLParagraphElement} */
+    let label;
+
+    onMount(() => {
+        label.style.marginBottom = "-" + (label.offsetHeight) + "px";
+        label.style.top = "-" + (label.offsetHeight) + "px";
+        label.style.left = "-" + ((image.offsetWidth / 2) - (label.offsetWidth / 2)) + "px";
+    }) 
+    
+</script>
+
+<div class="wrapper">
+    <img bind:this={image} src={src} alt="Hello there" class="portrait">
+    <p bind:this={label} class="label">{labelText}</p>
+</div>
+
+<style>
+    .wrapper {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .portrait {
+        width: 80%; /* Allow the image to scale proportionally */
+        max-width: 400px; /* Set an additional maximum width of 400px */
+    }
+
+    .label {
+        margin: 0;
+        position:relative;
+        writing-mode: vertical-lr;
+        text-orientation: upright;
+        color: #D4E2EF;
+        font-size: 30px;
+        font-family: 'Noto Serif JP', serif;
+        padding: 0;
+        padding-bottom: 5px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import TextPortrait from "$lib/components/TextPortrait.svelte";
+    import portrait from '$lib/assets/IMG_6281.jpg'
+</script>
+
+
+<div class="center">
+    <h1 class="first-name archivo royal">DAVID</h1>
+    <TextPortrait src={portrait} labelText="About"/>
+    <h1 class="last-name archivo royal">PREDA</h1>
+</div>
+
+<style>
+    .center {
+        display:flex;
+        height: 100%;
+        margin-left: auto;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .first-name, .last-name {
+        margin: 0;
+    }
+
+    .first-name {
+        margin-bottom: 50px;
+    }
+    
+    .last-name {
+        margin-top: 50px;
+    }
+
+    .archivo {
+        font-family: 'Big Shoulders Display', cursive;
+        font-size: 3em;
+        font-weight: 600;
+        letter-spacing: .1em;
+    }
+
+    .royal {
+        color: #163852;
+    }
+</style>
