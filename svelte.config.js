2f389e88b01e19f3b8c7cf5af964bd6dd6eeb911
David Preda <dtpreda30@gmail.com>
2023-09-25 11:55:32 +0100

    Update svelte config for GH pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 
-export default {
+const dev = process.argv.includes('dev');
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	}
 };
